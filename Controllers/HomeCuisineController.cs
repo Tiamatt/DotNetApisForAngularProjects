3509a4e997d90b133245c6a94667320ebe70eae9
tiamatt <samira.yusifova@gmail.com>
2018-10-08 02:11:32 -0400

    HomeCuisineApi migration

--- Controllers/HomeCuisineController.cs
+++ Controllers/HomeCuisineController.cs
@@ -19,21 +19,74 @@ namespace DotNetApisForAngularProjects.Controllers
             this.context = _context;
         }
 
-        // GET api/values
-        [HttpGet]
+        #region CRUD -> READ
+
+        // GET api/homecuisine
+        [HttpGet] // for testing
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "valueHC7", "value2", "valuetrt3" };
+            return new string[] { "HomeCuisine1", "HomeCuisine2", "HomeCuisine3" };
         }
 
-        // GET api/homecuisine/measure
-        [HttpGet("measure")]
+        // GET api/homecuisine/measures
+        [HttpGet("measures")]
         [ProducesResponseType(typeof(IEnumerable<Measure>), 200)]
         public async Task<IActionResult> GetMeasures()
         {
-            var res = await context.Measure.ToListAsync();
+            var res = await context.Measure
+            .Where(x => x.Active == true)
+            .OrderBy(y=> y.Name)
+            .ToListAsync();
 
             return Ok(res);
         }
+
+        // GET api/homecuisine/ingredients
+        [HttpGet("ingredients")]
+        [ProducesResponseType(typeof(IEnumerable<Error>), 200)]
+        public async Task<IActionResult> GetIngredients()
+        {
+            var res = await context.Ingredient
+            .ToListAsync();
+            
+            return Ok(res);
+        }
+
+        // GET api/homecuisine/errors
+        [HttpGet("errors")]
+        [ProducesResponseType(typeof(IEnumerable<Error>), 200)]
+        public async Task<IActionResult> GetErrors()
+        {
+            var res = await context.Error
+            .ToListAsync();
+            
+            return Ok(res);
+        }
+
+        #endregion
+
+        #region CRUD -> CREATE
+
+        // POST api/homecuisine/ingredient
+        [HttpPost]
+        [Route("ingredient")]
+        public async Task<IActionResult> CreateIngredient([FromBody]Ingredient model)
+        {
+            context.Ingredient.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        // POST api/homecuisine/error
+        [HttpPost]
+        [Route("error")]
+        public async Task<IActionResult> CreateError([FromBody]Error model)
+        {
+            context.Error.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        #endregion
     }
 }
